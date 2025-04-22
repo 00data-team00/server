524465b0caf3a2aedb67af3210f8a24e5294f425
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 22:31:51 +0900

    feat: like entity

---
+++ src/main/java/com/_data/_data/community/entity/Like.java
@@ -0,0 +1,37 @@
+package com._data._data.community.entity;
+
+import com._data._data.user.entity.Users;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"post_id","user_id"}))
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    private LocalDateTime createdAt;
+}
