e43b68164f275d8e3ae6631dfa6133711c9d7146
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 18:18:45 +0900

    feat: mail controller

---
+++ src/main/java/com/_data/_data/auth/controller/MailController.java
@@ -0,0 +1,44 @@
+package com._data._data.auth.controller;
+
+import com._data._data.auth.dto.MailAuthRequest;
+import com._data._data.auth.dto.MailSendRequest;
+import com._data._data.common.dto.ApiResponse;
+import com._data._data.auth.service.MailService;
+import com._data._data.user.service.UserService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/mail")
+@RequiredArgsConstructor
+public class MailController {
+
+    private final UserService userService;
+
+    @PostMapping("/send-auth-code")
+    public ApiResponse sendAuthCode(
+        @RequestBody MailSendRequest request
+    ) throws MessagingException {
+        boolean isSent = userService.sendAuthcode(request.email());
+        return new ApiResponse(
+            isSent,
+            isSent ? "이메일로 인증 코드가 전송되었습니다." : "이메일 인증 코드 발급에 실패하였습니다."
+        );
+    }
+
+    @PostMapping("/auth")
+    public ApiResponse validateAuthCode(
+        @RequestBody MailAuthRequest request
+    ) {
+        boolean isSuccess = userService.validationAuthcode(
+            request.email(),
+            request.verificationCode()
+        );
+        return new ApiResponse(
+            isSuccess,
+            isSuccess ? "이메일 인증에 성공하였습니다."
+                : "이메일 인증에 실패하였습니다."
+        );
+    }
+}
