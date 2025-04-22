859f6fccac1654d062c143eed245603ea60f9189
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 18:43:52 +0900

    feat: application에 스케줄러 사용설정

--- src/main/java/com/_data/_data/Application.java
+++ src/main/java/com/_data/_data/Application.java
@@ -3,7 +3,9 @@ package com._data._data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication()
 @EnableJpaRepositories(basePackages = "com._data._data")
 public class Application {
