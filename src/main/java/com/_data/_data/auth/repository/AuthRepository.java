4d235ddbb804c38983ad18810ac7379c9cf47655
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 18:17:04 +0900

    feat: auth repository

---
+++ src/main/java/com/_data/_data/auth/repository/AuthRepository.java
@@ -0,0 +1,11 @@
+package com._data._data.auth.repository;
+
+import com._data._data.auth.entity.Auth;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+    Auth findByEmail(String email);
+}
