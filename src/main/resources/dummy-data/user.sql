48c0b8e2becc3f739757aedf01a0de300b1f7518
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 15:28:29 +0900

    feat: ddl=update, 더미데이터 중복추가방지

--- src/main/resources/dummy-data/user.sql
+++ src/main/resources/dummy-data/user.sql
@@ -1,13 +1,12 @@
 -- src/main/resources/dummy-data.sql
 
--- 1) 한국(410), 미국(840), 일본(392), 중국(156), 캐나다(124) 등 ISO 국가번호 사용 예시
-INSERT INTO users (email, nations) VALUES ('alice.kim@example.com', 410);
-INSERT INTO users (email, nations) VALUES ('bob.lee@example.com',   840);
-INSERT INTO users (email, nations) VALUES ('charlie.yamamoto@example.jp', 392);
-INSERT INTO users (email, nations) VALUES ('david.zhang@example.cn', 156);
-INSERT INTO users (email, nations) VALUES ('emily.clark@example.ca', 124);
-
--- 2) 추가 더미 데이터
-INSERT INTO users (email, nations) VALUES ('frank.park@example.com', 410);
-INSERT INTO users (email, nations) VALUES ('grace.hwang@example.com', 410);
-INSERT INTO users (email, nations) VALUES ('helen.choi@example.com', 840);
+-- 이미 존재하면 삽입을 무시하도록 INSERT IGNORE 사용
+INSERT IGNORE INTO users (email, nations) VALUES
+    ('alice.kim@example.com',               410),
+    ('bob.lee@example.com',                 840),
+    ('charlie.yamamoto@example.jp',         392),
+    ('david.zhang@example.cn',              156),
+    ('emily.clark@example.ca',              124),
+    ('frank.park@example.com',              410),
+    ('grace.hwang@example.com',             410),
+    ('helen.choi@example.com',              840);
