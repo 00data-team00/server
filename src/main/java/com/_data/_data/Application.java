b2c1bcd3ff500dcdb8bfec1560a3756b0b515999
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 18:16:37 +0900

    fix: main application repository 스캔 범위 지정

--- src/main/java/com/_data/_data/Application.java
+++ src/main/java/com/_data/_data/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
-@EnableJpaRepositories(basePackages = "com._data._data.aichat.repository")
+@EnableJpaRepositories(basePackages = "com._data._data")
 public class Application {
 
 	public static void main(String[] args) {
