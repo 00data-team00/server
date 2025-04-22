f83952a8f97c3683c1edde9799b39546ce05ef7f
YinaePark <dlsoso1@unist.ac.kr>
2025-04-22 18:18:35 +0900

    feat: mail 관련 user service

---
+++ src/main/java/com/_data/_data/user/service/UserService.java
@@ -0,0 +1,41 @@
+package com._data._data.user.service;
+
+import com._data._data.auth.entity.Auth;
+import com._data._data.auth.repository.AuthRepository;
+import com._data._data.auth.service.MailService;
+import com._data._data.user.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final MailService mailService;
+    private final AuthRepository authRepository;
+
+    @Transactional
+    public boolean sendAuthcode(String email) throws MessagingException {
+        String authCode = mailService.sendSimpleMessage(email);
+        if(authCode != null){
+            Auth auth = authRepository.findByEmail(email);
+            if(auth == null)
+                authRepository.save(new Auth(email, authCode));
+            else
+                auth.patch(authCode);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validationAuthcode(String email, String authCode) {
+        Auth auth = authRepository.findByEmail(email);
+        if(auth != null && auth.getAuthCode().equals(authCode)){
+            authRepository.delete(auth);
+            return true;
+        }
+        return false;
+    }
+}
