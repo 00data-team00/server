e5fd40c301055aba2ef4695280b3f9c747448fbe
YinaePark <dlsoso1@unist.ac.kr>
2025-04-23 14:35:44 +0900

    feat: common exception

---
+++ src/main/java/com/_data/_data/common/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com._data._data.common.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
