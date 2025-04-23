91255e58bdf597176cfd4d6bf38d9465cd13d9cc
YinaePark <dlsoso1@unist.ac.kr>
2025-04-23 22:27:19 +0900

    chore: 주석 제거

--- src/main/java/com/_data/_data/auth/repository/AuthRepository.java
+++ src/main/java/com/_data/_data/auth/repository/AuthRepository.java
@@ -9,7 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuthRepository extends JpaRepository<Auth, Long> {
     Auth findByEmail(String email);
-
-    // 특정 시각 이전에 만료된 코드를 한 번에 삭제
     long deleteByExpiresAtBefore(LocalDateTime time);
 }
