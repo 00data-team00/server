6443da71530baaeb41f8abfd7de3f4c1fabef3eb
YinaePark <dlsoso1@unist.ac.kr>
2025-04-27 16:24:58 +0900

    feat: post controller 이미지 업로드

--- src/main/java/com/_data/_data/community/controller/PostController.java
+++ src/main/java/com/_data/_data/community/controller/PostController.java
@@ -1,13 +1,14 @@
 package com._data._data.community.controller;
 
 import com._data._data.auth.entity.CustomUserDetails;
+import com._data._data.common.dto.ApiResponse;
 import com._data._data.community.dto.CommentDto;
-import com._data._data.community.dto.PostCreateRequest;
 import com._data._data.community.dto.PostDto;
-import com._data._data.community.dto.ProfileDto;
 import com._data._data.community.service.PostService;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/me/posts")
@@ -24,12 +27,13 @@ public class PostController {
     private final PostService postService;
 
     // === 포스트 작성 ===
-    @PostMapping("")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public PostDto createPost(
         @AuthenticationPrincipal CustomUserDetails principal,
-        @RequestBody PostCreateRequest request
-    ) {
-        return postService.createPost(principal.getUser(), request);
+        @RequestPart("content") String content,
+        @RequestPart(value = "image", required = false) MultipartFile image
+    ) throws IOException {
+        return postService.createPost(principal.getUser(), content, image);
     }
 
     // === 내가 작성한 포스트 조회 ===
@@ -84,6 +88,14 @@ public class PostController {
         return postService.getNationTimeline(principal.getUser());
     }
 
-
+    // 포스트 삭제
+    @DeleteMapping("/{postId}")
+    public ApiResponse deletePost(
+        @AuthenticationPrincipal CustomUserDetails principal,
+        @PathVariable Long postId
+    ) {
+        postService.deletePost(principal.getUser(), postId);
+        return new ApiResponse(true, "포스트 삭제 성공");
+    }
 
 }
