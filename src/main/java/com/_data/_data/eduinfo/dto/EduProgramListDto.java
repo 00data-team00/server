ccc8405e7f91d0d9bb68e5a86fb90974c1e1b9aa
kmj7769 <rlaalswns7769@gmail.com>
2025-04-30 15:34:10 +0900

    list to listDto

---
+++ src/main/java/com/_data/_data/eduinfo/dto/EduProgramListDto.java
@@ -0,0 +1,10 @@
+package com._data._data.eduinfo.dto;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class EduProgramListDto {
+    private List<EduProgramSimpleDto> eduPrograms;
+}
