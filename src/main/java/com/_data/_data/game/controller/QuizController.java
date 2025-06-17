77da7f69a4b93835fcb13f08a7d422d91d778061
kmj7769 <rlaalswns7769@gmail.com>
2025-06-17 17:04:02 +0900

    swagger config for quiz api added

--- src/main/java/com/_data/_data/game/controller/QuizController.java
+++ src/main/java/com/_data/_data/game/controller/QuizController.java
@@ -3,6 +3,8 @@ package com._data._data.game.controller;
 import com._data._data.game.dto.QuizDto;
 import com._data._data.game.entity.Quiz;
 import com._data._data.game.service.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/game")
 @RequiredArgsConstructor
+@Tag(name = "Quiz", description = "게이미피케이션 퀴즈 관련 API")
 public class QuizController {
 
     private final QuizService quizService;
 
     @GetMapping("/me/quiz")
+    @Operation(
+            summary = "퀴즈 정보 불러오기",
+            description = "해당 ID의 퀴즈 정보를 반환합니다."
+    )
     public QuizDto getQuizById(@RequestParam Long id) {
         Quiz quiz = quizService.getQuiz(id);
 
