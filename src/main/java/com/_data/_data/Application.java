bbee05c41a9a16d9118f2d948f040da1d70bf771
YinaePark <dlsoso1@unist.ac.kr>
2025-06-24 17:48:46 +0900

    feat: 비동기 처리 위해 어노테이션 추가

--- src/main/java/com/_data/_data/Application.java
+++ src/main/java/com/_data/_data/Application.java
@@ -3,8 +3,10 @@ package com._data._data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication()
 @EnableJpaRepositories(basePackages = "com._data._data")
