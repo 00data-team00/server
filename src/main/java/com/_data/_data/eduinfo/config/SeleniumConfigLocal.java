01ae67a92bb2134501721386819f4bbf9587f873
YinaePark <dlsoso1@unist.ac.kr>
2025-06-25 03:52:15 +0900

    fix: 웹 드라이버 설정 프로필별 설정 분리

--- src/main/java/com/_data/_data/eduinfo/config/SeleniumConfigLocal.java
+++ src/main/java/com/_data/_data/eduinfo/config/SeleniumConfigLocal.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("local")
+@Profile("!prod")
 public class SeleniumConfigLocal {
     @Bean(destroyMethod = "quit")
     public WebDriver webDriver() {
