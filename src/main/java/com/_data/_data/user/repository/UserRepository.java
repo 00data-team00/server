2bb0548596e0c3d9c8dde295b73a759234ce4d99
YinaePark <dlsoso1@unist.ac.kr>
2025-06-25 18:47:23 +0900

    feat: user repository deleted 플래그 조건 추가

--- src/main/java/com/_data/_data/user/repository/UserRepository.java
+++ src/main/java/com/_data/_data/user/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com._data._data.user.repository;
 
 import com._data._data.user.entity.Users;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
-    boolean existsByEmail(String email);
-    Users findByEmail(String email);
+    boolean existsByEmailAndIsDeletedFalse(String email);
+    Users findByEmailAndIsDeletedFalse(String email);
+    Optional<Users> findByEmailAndIsDeletedTrue(String email);
 }
