a3f7886a15c8a6ffdfc950857b62ea90641ac58d
YinaePark <dlsoso1@unist.ac.kr>
2025-06-25 16:24:31 +0900

    feat: 로컬 브랜치에서 eduprog 링크 업데이트용 커밋

---
+++ src/main/java/com/_data/_data/eduinfo/config/SeleniumConfigLocal.java
@@ -0,0 +1,22 @@
+package com._data._data.eduinfo.config;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!prod")
+public class SeleniumConfigLocal {
+    @Bean(destroyMethod = "quit")
+    public WebDriver webDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions opts = new ChromeOptions();
+        opts.addArguments("--headless","--no-sandbox","--disable-dev-shm-usage");
+        return new ChromeDriver(opts);
+    }
+}
