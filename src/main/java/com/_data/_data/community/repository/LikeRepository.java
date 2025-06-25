cf798e9ca952b495ef0c1aae9d34936f07aebef4
YinaePark <dlsoso1@unist.ac.kr>
2025-06-25 18:48:23 +0900

    feat: 회원탈퇴기능 관련 커뮤니티 레포지토리 함수 추가

--- src/main/java/com/_data/_data/community/repository/LikeRepository.java
+++ src/main/java/com/_data/_data/community/repository/LikeRepository.java
@@ -5,11 +5,18 @@ import com._data._data.community.entity.Post;
 import com._data._data.user.entity.Users;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsByPostAndUser(Post post, Users user);
     Optional<Like> findByPostAndUser(Post post, Users user);
     void deleteByPost(Post post);
+
+    // 사용자가 누른 모든 좋아요 삭제
+    @Modifying
+    @Transactional
+    void deleteByUser(Users user);
 }
