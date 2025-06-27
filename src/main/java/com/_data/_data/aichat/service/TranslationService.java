0bd32d54689a7f79b400dbf0f3561b0fd13e15a6
kmj7769 <rlaalswns7769@gmail.com>
2025-06-27 22:52:52 +0900

    translateText request modified

--- src/main/java/com/_data/_data/aichat/service/TranslationService.java
+++ src/main/java/com/_data/_data/aichat/service/TranslationService.java
@@ -1,11 +1,14 @@
 package com._data._data.aichat.service;
 
+import com._data._data.aichat.dto.TranslateRequestDto;
 import com._data._data.aichat.entity.Translation;
 
 public interface TranslationService {
     Translation translateMessage(Long messageId, String messageText, String targetLang) throws Exception;
 
-    Translation getTranslation(Long translationId) throws Exception;
+    void getTranslation(Long translationId) throws Exception;
+
+    Translation getTranslation(TranslateRequestDto translateRequestDto) throws Exception;
 
     /**
      * @param text 번역할 원문
