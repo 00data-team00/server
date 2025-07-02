0f2f17ff42aa3d4749e8414a22657b6795c60d15
YinaePark <dlsoso1@unist.ac.kr>
2025-07-03 02:10:18 +0900

    feat: 국가 entity 생성

---
+++ src/main/java/com/_data/_data/user/entity/Nation.java
@@ -0,0 +1,31 @@
+package com._data._data.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "nations")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Nation {
+    @Id
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 3)
+    private String code;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "name_ko", nullable = false, length = 100)
+    private String nameKo;
+}
