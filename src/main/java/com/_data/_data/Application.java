a3db140ed0de8e47ae55fc533532147303b6f6d6
kmj7769 <155081737+kmj7769@users.noreply.github.com>
2025-07-03 15:35:32 +0900

    Update Application.java
    set timezone "Asiz/Seoul"

--- src/main/java/com/_data/_data/Application.java
+++ src/main/java/com/_data/_data/Application.java
@@ -1,10 +1,13 @@
 package com._data._data;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableAsync
 @EnableScheduling
