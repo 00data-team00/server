ca1301a6a9786b7a046593333711b0f14d9bbec1
YinaePark <dlsoso1@unist.ac.kr>
2025-07-14 00:20:04 +0900

    feat: refresh 토큰 레포지토리

---
+++ src/main/java/com/_data/_data/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com._data._data.auth.repository;
+
+
+import com._data._data.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByUserId(Long userId);
+    void deleteByUserId(Long userId);
+    void deleteByToken(String token);
+}
